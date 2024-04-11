edc6d819d9a484709affaa2254658910c54eb30b
lnsqj123 <holmane33@gmail.com>
2024-04-12 01:15:31 +0900

    [back] feat: create total ranking API, fix point API

--- server/point.py
+++ server/point.py
@@ -15,7 +15,9 @@ Point = Namespace(
 point_check_fields = Point.model(
     "Point_check_request",
     {  # Model 객체 생성
-        "user_id": fields.String(description="an ID", required=True, example="ID"),
+        "access_token": fields.String(
+            description="an access token", required=True, example="access_token"
+        )
     },
 )
 point_check_response1_data = Point.model(
@@ -30,10 +32,17 @@ point_check_response1 = Point.model(
     },
 )
 point_check_response2 = Point.model(
-    "Point_check_fail",
+    "Point_check_fail1",
     {
         "result": fields.String(example="fail"),
         "msg": fields.String(example="포인트 확인에 실패하였습니다."),
+    },
+)
+point_check_response3 = Point.model(
+    "Point_check_fail2",
+    point_check_response2,
+    {
+        "msg": fields.String(example="access token이 유효하지 않습니다."),
     },
 )
 
@@ -44,66 +53,7 @@ class check_point(Resource):
     @Point.expect(point_check_fields)
     @Point.response(202, "success", point_check_response1)
     @Point.response(401, "fail", point_check_response2)
-    def post(self):
-        """현재 포인트를 얼마나 가지고 있는지를 확인합니다 - id 사용"""
-        req = request.get_json()  # api를 요청한 서버에서 보낸 json파일 저장
-        id_receive = req["user_id"]
-        try:
-            # select 쿼리 실행: request의 name과 동일한 document 찾기
-            result = db.user.find_one({"user_id": id_receive}, {"_id": 0, "point": 1})
-            # 데이터를 처리하고 응답 생성
-            response_data = {
-                "result": "success",
-                "msg": "포인트 확인에 성공하였습니다.",
-                "data": result,
-            }
-            return make_response(jsonify(response_data), 202)
-        except:
-            response_data = {"result": "fail", "msg": "포인트 확인에 실패하였습니다."}
-            return make_response(jsonify(response_data), 401)
-
-
-point_check_via_token_fields = Point.model(
-    "Point_check_via_token_request",
-    {  # Model 객체 생성
-        "access_token": fields.String(
-            description="an access token", required=True, example="access_token"
-        )
-    },
-)
-point_check_via_token_response1_data = Point.model(
-    "Point_check_via_token_response1_data", {"point": fields.Integer(example=30)}
-)
-point_check_via_token_response1 = Point.inherit(
-    "Point_check_via_token_success",
-    point_check_response1,
-    {
-        "data": fields.Nested(point_check_via_token_response1_data),
-    },
-)
-point_check_via_token_response2 = Point.inherit(
-    "Point_check_via_token_fail1",
-    point_check_response2,
-    {
-        "msg": fields.String(example="포인트 적립에 실패하였습니다."),
-    },
-)
-point_check_via_token_response3 = Point.inherit(
-    "Point_check_via_token_fail2",
-    point_check_response2,
-    {
-        "msg": fields.String(example="access token이 유효하지 않습니다."),
-    },
-)
-
-
-# point add API call route
-@Point.route("/checkViaToken", methods=["POST"])
-class check_point_via_token(Resource):
-    @Point.expect(point_check_via_token_fields)
-    @Point.response(202, "success", point_check_via_token_response1)
-    @Point.response(401, "fail", point_check_via_token_response2)
-    @Point.response(402, "fail", point_check_via_token_response3)
+    @Point.response(402, "fail", point_check_response3)
     def post(self):
         """현재 포인트를 얼마나 가지고 있는지를 확인합니다 - token 사용"""
         req = request.get_json()
@@ -130,10 +80,62 @@ class check_point_via_token(Resource):
             return make_response(jsonify(response_data), 401)
 
 
-point_add_fields = Point.inherit(
-    "Point_add_request",
-    point_check_fields,
+point_check_using_id_fields = Point.model(
+    "Point_check_using_id_request",
     {  # Model 객체 생성
+        "user_id": fields.String(description="an ID", required=True, example="ID"),
+    },
+)
+point_check_using_id_response1_data = Point.model(
+    "Point_check_using_id_response1_data", {"point": fields.Integer(example=30)}
+)
+point_check_using_id_response1 = Point.inherit(
+    "Point_check_using_id_success",
+    point_check_response1,
+    {
+        "msg": fields.String(example="포인트 확인에 성공하였습니다."),
+    },
+)
+point_check_using_id_response2 = Point.inherit(
+    "Point_check_using_id_fail",
+    point_check_response2,
+    {
+        "msg": fields.String(example="포인트 확인에 실패하였습니다."),
+    },
+)
+
+
+# point check API call route
+@Point.route("/checkUsingId", methods=["POST"])
+class check_point_using_id(Resource):
+    @Point.expect(point_check_using_id_fields)
+    @Point.response(202, "success", point_check_using_id_response1)
+    @Point.response(401, "fail", point_check_using_id_response2)
+    def post(self):
+        """현재 포인트를 얼마나 가지고 있는지를 확인합니다 - id 사용"""
+        req = request.get_json()  # api를 요청한 서버에서 보낸 json파일 저장
+        id_receive = req["user_id"]
+        try:
+            # select 쿼리 실행: request의 name과 동일한 document 찾기
+            result = db.user.find_one({"user_id": id_receive}, {"_id": 0, "point": 1})
+            # 데이터를 처리하고 응답 생성
+            response_data = {
+                "result": "success",
+                "msg": "포인트 확인에 성공하였습니다.",
+                "data": result,
+            }
+            return make_response(jsonify(response_data), 202)
+        except:
+            response_data = {"result": "fail", "msg": "포인트 확인에 실패하였습니다."}
+            return make_response(jsonify(response_data), 401)
+
+
+point_add_fields = Point.model(
+    "Point_add_request",
+    {  # Model 객체 생성
+        "access_token": fields.String(
+            description="an access token", required=True, example="access_token"
+        ),
         "point": fields.Integer(description="a point", required=True, example=20),
     },
 )
@@ -148,10 +150,17 @@ point_add_response1 = Point.inherit(
     },
 )
 point_add_response2 = Point.inherit(
-    "Point_add_fail",
+    "Point_add_fail1",
     point_check_response2,
     {
         "msg": fields.String(example="포인트 적립에 실패하였습니다."),
+    },
+)
+point_add_response3 = Point.inherit(
+    "Point_add_fail2",
+    point_check_response2,
+    {
+        "msg": fields.String(example="access token이 유효하지 않습니다."),
     },
 )
 
@@ -162,12 +171,20 @@ class add_point(Resource):
     @Point.expect(point_add_fields)
     @Point.response(202, "success", point_add_response1)
     @Point.response(401, "fail", point_add_response2)
+    @Point.response(402, "fail", point_add_response3)
     def post(self):
-        """요청한 포인트만큼 적립합니다 - id 사용"""
+        """요청한 포인트만큼 적립합니다 - token 사용"""
         req = request.get_json()
-        id_receive = req["user_id"]
+        token_receive = req["access_token"]
         point_receive = req["point"]
+        payload = check_access_token(token_receive)
+        if payload == None:
+            return make_response(
+                jsonify({"result": "fail", "msg": "access token이 유효하지 않습니다."}),
+                402,
+            )
         try:
+            id_receive = payload["id"]
             # find one and update 쿼리 실행: request의 name과 동일한 document에서 score 속성 값을 기존 값에서 request의 score 값만큼 증가(inc), 업데이트 이후에 값을 return
             result = db.user.find_one_and_update(
                 {"user_id": id_receive},
@@ -187,61 +204,44 @@ class add_point(Resource):
             return make_response(jsonify(response_data), 401)
 
 
-point_add_via_token_fields = Point.model(
-    "Point_add_via_token_request",
+point_add_using_id_fields = Point.inherit(
+    "Point_add_using_id_request",
+    point_check_using_id_fields,
     {  # Model 객체 생성
-        "access_token": fields.String(
-            description="an access token", required=True, example="access_token"
-        ),
         "point": fields.Integer(description="a point", required=True, example=20),
     },
 )
-point_add_via_token_response1_data = Point.model(
-    "Point_add_via_token_response1_data", {"point": fields.Integer(example=30)}
+point_add_using_id_response1_data = Point.model(
+    "Point_add_using_id_response1_data", {"point": fields.Integer(example=30)}
 )
-point_add_via_token_response1 = Point.inherit(
-    "Point_add_via_token_success",
-    point_check_response1,
+point_add_using_id_response1 = Point.inherit(
+    "Point_add_using_id_success",
+    point_check_using_id_response1,
     {
-        "data": fields.Nested(point_add_via_token_response1_data),
+        "data": fields.Nested(point_add_response1_data),
     },
 )
-point_add_via_token_response2 = Point.inherit(
-    "Point_add_via_token_fail1",
-    point_check_response2,
+point_add_using_id_response2 = Point.inherit(
+    "Point_add_using_id_fail",
+    point_check_using_id_response2,
     {
         "msg": fields.String(example="포인트 적립에 실패하였습니다."),
-    },
-)
-point_add_via_token_response3 = Point.inherit(
-    "Point_add_via_token_fail2",
-    point_check_response2,
-    {
-        "msg": fields.String(example="access token이 유효하지 않습니다."),
     },
 )
 
 
 # point add API call route
-@Point.route("/addViaToken", methods=["POST"])
-class add_point_via_token(Resource):
-    @Point.expect(point_add_via_token_fields)
-    @Point.response(202, "success", point_add_via_token_response1)
-    @Point.response(401, "fail", point_add_via_token_response2)
-    @Point.response(402, "fail", point_add_via_token_response3)
+@Point.route("/addUsingId", methods=["POST"])
+class add_point_using_id(Resource):
+    @Point.expect(point_add_using_id_fields)
+    @Point.response(202, "success", point_add_using_id_response1)
+    @Point.response(401, "fail", point_add_using_id_response2)
     def post(self):
-        """요청한 포인트만큼 적립합니다 - token 사용"""
+        """요청한 포인트만큼 적립합니다 - id 사용"""
         req = request.get_json()
-        token_receive = req["access_token"]
+        id_receive = req["user_id"]
         point_receive = req["point"]
-        payload = check_access_token(token_receive)
-        if payload == None:
-            return make_response(
-                jsonify({"result": "fail", "msg": "access token이 유효하지 않습니다."}),
-                402,
-            )
         try:
-            id_receive = payload["id"]
             # find one and update 쿼리 실행: request의 name과 동일한 document에서 score 속성 값을 기존 값에서 request의 score 값만큼 증가(inc), 업데이트 이후에 값을 return
             result = db.user.find_one_and_update(
                 {"user_id": id_receive},
--- server/ranking.py
+++ server/ranking.py
@@ -1,7 +1,8 @@
 # from flask_cors import CORS # CORS 미들웨어를 위한 모듈
-from flask import request, jsonify  # flask 관련 라이브러리
-from flask_restx import Resource, Namespace  # Api 구현을 위한 Api 객체 import
+from flask import request, jsonify, make_response  # flask 관련 라이브러리
+from flask_restx import Resource, Namespace, fields  # Api 구현을 위한 Api 객체 import
 from database import db
+from pymongo import ReturnDocument
 from tokens import *
 
 
@@ -12,44 +13,328 @@ Ranking = Namespace(
 )
 
 
-# ranking check API call route
+ranking_check_fields = Ranking.model(
+    "Ranking_check_request",
+    {  # Model 객체 생성
+        "access_token": fields.String(
+            description="an access token", required=True, example="access_token"
+        )
+    },
+)
+ranking_check_response1_data = Ranking.model(
+    "Ranking_check_response1_data", {"score": fields.Integer(example=30)}
+)
+ranking_check_response1 = Ranking.model(
+    "Ranking_check_success",
+    {
+        "result": fields.String(example="success"),
+        "msg": fields.String(example="점수 확인에 성공하였습니다."),
+        "data": fields.Nested(ranking_check_response1_data),
+    },
+)
+ranking_check_response2 = Ranking.model(
+    "Ranking_check_fail1",
+    {
+        "result": fields.String(example="fail"),
+        "msg": fields.String(example="점수 확인에 실패하였습니다."),
+    },
+)
+ranking_check_response3 = Ranking.model(
+    "Ranking_check_fail2",
+    {
+        "result": fields.String(example="fail"),
+        "msg": fields.String(example="access token이 유효하지 않습니다."),
+    },
+)
+
+
+# point add API call route
 @Ranking.route("/check", methods=["POST"])
-class check_ranking(Resource):
+class check_score(Resource):
+    @Ranking.expect(ranking_check_fields)
+    @Ranking.response(202, "success", ranking_check_response1)
+    @Ranking.response(401, "fail", ranking_check_response2)
+    @Ranking.response(402, "fail", ranking_check_response3)
     def post(self):
-        """현재 랭킹이 어떤지를 가져옵니다"""
-        data1 = request.get_json()  # api를 요청한 서버에서 보낸 json파일 저장
-        data2 = db.people.find_one(
-            {"name": data1["name"]}
-        )  # select 쿼리 실행: request의 name과 동일한 document 찾기
-        if data2 != None:
-            data2["_id"] = str(
-                data2["_id"]
-            )  # 찾은 값이 있을 때만 id를 Objectid에서 str로 변경
-        # 데이터를 처리하고 응답 생성
-        response_data = {
-            "message": "Data received successfully",
-            "received_data": data1,
-            "result_one": data2,
-        }  # 전달할 데이터 묶기
-        return jsonify(response_data)  # json형태로 전달
+        """현재 점수를 얼마나 가지고 있는지를 확인합니다 - token 사용"""
+        req = request.get_json()
+        token_receive = req["access_token"]
+        payload = check_access_token(token_receive)
+        if payload == None:
+            return make_response(
+                jsonify({"result": "fail", "msg": "access token이 유효하지 않습니다."}),
+                402,
+            )
+        try:
+            id_receive = payload["id"]
+            # select 쿼리 실행: request의 id과 동일한 document 찾기
+            result = db.user.find_one({"user_id": id_receive}, {"_id": 0, "score": 1})
+            # 데이터를 처리하고 응답 생성
+            response_data = {
+                "result": "success",
+                "msg": "점수 확인에 성공하였습니다.",
+                "data": result,
+            }
+            return make_response(jsonify(response_data), 202)
+        except:
+            response_data = {"result": "fail", "msg": "점수 확인에 실패하였습니다."}
+            return make_response(jsonify(response_data), 401)
 
 
-# ranking add API call route
+ranking_add_fields = Ranking.model(
+    "Ranking_add_request",
+    {  # Model 객체 생성
+        "access_token": fields.String(
+            description="an access token", required=True, example="access_token"
+        ),
+        "score": fields.Integer(description="a score", required=True, example=20),
+    },
+)
+ranking_add_response1_data = Ranking.model(
+    "Ranking_add_response1_data", {"score": fields.Integer(example=30)}
+)
+ranking_add_response1 = Ranking.inherit(
+    "Ranking_add_success",
+    ranking_check_response1,
+    {
+        "msg": fields.String(example="점수 적립에 성공하였습니다."),
+        "data": fields.Nested(ranking_add_response1_data),
+    },
+)
+ranking_add_response2 = Ranking.inherit(
+    "Ranking_add_fail1",
+    ranking_check_response2,
+    {
+        "msg": fields.String(example="점수 적립에 실패하였습니다."),
+    },
+)
+ranking_add_response3 = Ranking.inherit(
+    "Ranking_add_fail2",
+    ranking_check_response2,
+    {
+        "msg": fields.String(example="access token이 유효하지 않습니다."),
+    },
+)
+
+
+# point add API call route
 @Ranking.route("/add", methods=["POST"])
-class add_ranking(Resource):
+class add_score(Resource):
+    @Ranking.expect(ranking_add_fields)
+    @Ranking.response(202, "success", ranking_add_response1)
+    @Ranking.response(401, "fail", ranking_add_response2)
+    @Ranking.response(402, "fail", ranking_add_response3)
     def post(self):
-        """랭킹에 적용될 포인트를 추가합니다"""
-        data1 = request.get_json()
-        data3 = db.people.update_one(
-            {"name": data1["name"]}, {"$inc": {"score": data1["score"]}}
-        )  # update 쿼리 실행: request의 name과 동일한 document에서 score 속성 값을 기존 값에서 request의 score 값만큼 증가(inc)
-        data2 = db.people.find_one({"name": data1["name"]})
-        if data2 != None:
-            data2["_id"] = str(data2["_id"])
-        # 데이터를 처리하고 응답 생성
-        response_data = {
-            "message": "Data received successfully",
-            "received_data": data1,
-            "result_one": data2,
-        }
-        return jsonify(response_data)
+        """요청한 점수만큼 적립합니다 - token 사용"""
+        req = request.get_json()
+        token_receive = req["access_token"]
+        score_receive = req["score"]
+        payload = check_access_token(token_receive)
+        if payload == None:
+            return make_response(
+                jsonify({"result": "fail", "msg": "access token이 유효하지 않습니다."}),
+                402,
+            )
+        try:
+            id_receive = payload["id"]
+            # find one and update 쿼리 실행: request의 name과 동일한 document에서 score 속성 값을 기존 값에서 request의 score 값만큼 증가(inc), 업데이트 이후에 값을 return
+            result = db.user.find_one_and_update(
+                {"user_id": id_receive},
+                {"$inc": {"score": score_receive}},
+                projection={"_id": 0, "score": 1},
+                return_document=ReturnDocument.AFTER,
+            )
+            # 데이터를 처리하고 응답 생성
+            response_data = {
+                "result": "success",
+                "msg": "점수 적립에 성공하였습니다.",
+                "data": result,
+            }
+            return make_response(jsonify(response_data), 202)
+        except:
+            response_data = {"result": "fail", "msg": "점수 적립에 실패하였습니다."}
+            return make_response(jsonify(response_data), 401)
+
+
+ranking_individual_fields = Ranking.model(
+    "Ranking_individual_request",
+    {},
+)
+ranking_individual_response1_data = Ranking.model(
+    "Ranking_individual_response1_data",
+    {
+        "rank": fields.Integer(),
+        "score": fields.Integer(),
+        "user_id": fields.String(),
+    },
+)
+ranking_individual_response1_example = [
+    {"rank": 1, "score": 4520, "user_id": "11175"},
+    {"rank": 2, "score": 1000, "user_id": "56111"},
+    {"rank": 3, "score": 500, "user_id": "116561"},
+]
+ranking_individual_response1 = Ranking.inherit(
+    "Ranking_individual_success",
+    ranking_check_response1,
+    {
+        "msg": fields.String(example="개인 점수 집계에 성공하였습니다."),
+        "data": fields.List(
+            fields.Nested(ranking_individual_response1_data),
+            default=ranking_individual_response1_example,
+        ),
+    },
+)
+ranking_individual_response2 = Ranking.inherit(
+    "Ranking_individual_fail",
+    ranking_check_response2,
+    {
+        "msg": fields.String(example="개인 점수 집계에 실패하였습니다."),
+    },
+)
+"""
+ranking_individual_response3 = Ranking.inherit(
+    "Ranking_individual_fail2",
+    ranking_check_response2,
+    {
+        "msg": fields.String(example="access token이 유효하지 않습니다."),
+    },
+)
+"""
+
+
+@Ranking.route("/aggregation/individual")
+class ranking_individual(Resource):
+    @Ranking.expect(ranking_individual_fields)
+    @Ranking.response(202, "success", ranking_individual_response1)
+    @Ranking.response(401, "fail", ranking_individual_response2)
+    # @Ranking.response(402, "fail", ranking_add_response3)
+    def post(self):
+        """개인 점수 집계를 확인합니다 - token 사용"""
+        req = request.get_json()
+        """token_receive = req["access_token"]
+        score_receive = req["score"]
+        payload = check_access_token(token_receive)
+        if payload == None:
+            return make_response(
+                jsonify({"result": "fail", "msg": "access token이 유효하지 않습니다."}),
+                402,
+            )"""
+        try:
+            # id_receive = payload["id"]
+            # find one and update 쿼리 실행: request의 name과 동일한 document에서 score 속성 값을 기존 값에서 request의 score 값만큼 증가(inc), 업데이트 이후에 값을 return
+            pipeline = [
+                {
+                    "$setWindowFields": {
+                        "sortBy": {"score": -1},
+                        "output": {"rank": {"$rank": {}}},
+                    }
+                },
+                {"$project": {"_id": 0, "rank": 1, "score": 1, "user_id": 1}},
+            ]
+            result = list(db.user.aggregate(pipeline))
+            # 데이터를 처리하고 응답 생성
+            response_data = {
+                "result": "success",
+                "msg": "개인 점수 집계에 성공하였습니다.",
+                "data": result,
+            }
+            return make_response(jsonify(response_data), 202)
+        except:
+            response_data = {
+                "result": "fail",
+                "msg": "개인 점수 집계에 실패하였습니다.",
+            }
+            return make_response(jsonify(response_data), 401)
+
+
+ranking_group_fields = Ranking.model(
+    "Ranking_group_request",
+    {},
+)
+ranking_group_response1_data = Ranking.model(
+    "Ranking_group_response1_data",
+    {
+        "rank": fields.Integer(),
+        "total": fields.Integer(),
+        "region": fields.String(),
+    },
+)
+ranking_group_response1_example = [
+    {"rank": 1, "region": "Daegu", "total": 4700},
+    {"rank": 2, "region": "Busan", "total": 1590},
+    {"rank": 3, "region": "Seoul", "total": 670},
+]
+ranking_group_response1 = Ranking.inherit(
+    "Ranking_group_success",
+    ranking_check_response1,
+    {
+        "msg": fields.String(example="그룹 점수 집계에 성공하였습니다."),
+        "data": fields.List(
+            fields.Nested(ranking_group_response1_data),
+            default=ranking_group_response1_example,
+        ),
+    },
+)
+ranking_group_response2 = Ranking.inherit(
+    "Ranking_group_fail",
+    ranking_check_response2,
+    {
+        "msg": fields.String(example="그룹 점수 집계에 실패하였습니다."),
+    },
+)
+"""
+ranking_individual_response3 = Ranking.inherit(
+    "Ranking_individual_fail2",
+    ranking_check_response2,
+    {
+        "msg": fields.String(example="access token이 유효하지 않습니다."),
+    },
+)
+"""
+
+
+@Ranking.route("/aggregation/group")
+class ranking_group(Resource):
+    @Ranking.expect(ranking_group_fields)
+    @Ranking.response(202, "success", ranking_group_response1)
+    @Ranking.response(401, "fail", ranking_group_response2)
+    def post(self):
+        """그룹 점수 집계를 확인합니다 - token 사용"""
+        req = request.get_json()
+        """token_receive = req["access_token"]
+        score_receive = req["score"]
+        payload = check_access_token(token_receive)
+        if payload == None:
+            return make_response(
+                jsonify({"result": "fail", "msg": "access token이 유효하지 않습니다."}),
+                402,
+            )"""
+        try:
+            # id_receive = payload["id"]
+            # find one and update 쿼리 실행: request의 name과 동일한 document에서 score 속성 값을 기존 값에서 request의 score 값만큼 증가(inc), 업데이트 이후에 값을 return
+            pipeline = [
+                {"$group": {"_id": "$region", "total": {"$sum": "$score"}}},
+                {"$sort": {"total": -1, "_id": 1}},
+                {"$project": {"_id": 0, "region": "$_id", "total": 1}},
+            ]
+            result = list(db.user.aggregate(pipeline))
+            if len(result):
+                result[0]["rank"] = 1
+            rank = 1
+            for i in range(1, len(result)):
+                if result[i]["total"] != result[i - 1]["total"]:
+                    rank = i + 1
+                result[i]["rank"] = rank
+            # 데이터를 처리하고 응답 생성
+            response_data = {
+                "result": "success",
+                "msg": "지역 점수 집계에 성공하였습니다.",
+                "data": result,
+            }
+            return make_response(jsonify(response_data), 202)
+        except:
+            response_data = {
+                "result": "fail",
+                "msg": "지역 점수 집계에 실패하였습니다.",
+            }
+            return make_response(jsonify(response_data), 401)
