2ce7a5dcad6aeb352ec0731a4e0a3aead7694415
chayhan <kcw2205@naver.com>
2024-04-24 01:47:18 +0900

    [front] fix: Uploaded omitted `AuthStore.ts`

--- client/src/store/AuthStore.ts
+++ client/src/store/AuthStore.ts
@@ -2,7 +2,7 @@ import {create} from 'zustand';
 
 // 인터페이스 정의
 interface AuthState {
-  isLoggedIn: boolean;
+  isAuth: boolean;
   username: string | null;
   login: (username: string, password: string) => boolean;
   logout: () => void;
@@ -10,11 +10,14 @@ interface AuthState {
 
 // 초기 상태 정의
 const initialState: AuthState = {
-  isLoggedIn: false,
+  isAuth: false,
   username: null,
   login: () => true,
   logout: () => {},
 };
+
+// JWT 토큰 타임아웃 설정
+const TOKEN_TIMEOUT = 600*1000;
 
 // Zustand 상태 생성
 const useAuthStore = create<AuthState>((set) => ({
@@ -23,8 +26,9 @@ const useAuthStore = create<AuthState>((set) => ({
   login: (username: string, password: string) => {
     // 여기서는 간단하게 username이 'admin'이고 password가 'password'일 때만 로그인 성공으로 가정
     // 또한, 패킷을 뜯어도 안전하도록 해싱처리를 해야하는데, 그건 일단 나중에.
+    
     if (username === 'admin' && password === 'password') {
-      set({ isLoggedIn: true, username: username });
+      set({ isAuth: true, username: username });
       return true;
     } else {
       // 로그인 실패 처리
@@ -34,7 +38,7 @@ const useAuthStore = create<AuthState>((set) => ({
   },
   // 로그아웃 액션 정의
   logout: () => {
-    set({ isLoggedIn: false, username: null });
+    set({ isAuth: false, username: null });
   },
 }));
 
