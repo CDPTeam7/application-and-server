b436c307749962c76b3bf44a71692cb71b32c5f5
blu3piece <kcw2205@naver.com>
2024-04-26 01:04:00 +0900

    [front] fix: fix `BottomNav.tsx` view logic
    - fixed
    Bottom Navigation now tracks the router of client when it rerenders.

--- client/src/components/BottomNav.tsx
+++ client/src/components/BottomNav.tsx
@@ -1,12 +1,11 @@
 import { css } from "@linaria/core";
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import PersonIcon from '@mui/icons-material/Person';
-import HomeIcon from '@mui/icons-material/Home';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import StackedLineChartIcon from '@mui/icons-material/StackedLineChart';
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import PersonIcon from "@mui/icons-material/Person";
+import HomeIcon from "@mui/icons-material/Home";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import StackedLineChartIcon from "@mui/icons-material/StackedLineChart";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import { Link, useLocation } from "react-router-dom";
-import { useState } from "react";
 
 const tabBarStyle = css`
   position: fixed;
@@ -17,50 +16,52 @@ const tabBarStyle = css`
 
 export const tabBarRoutes = [
   {
-    path:"/notifications",
-    icon:<NotificationsIcon />,
-    label:"알림"
+    path: "/notifications",
+    icon: <NotificationsIcon />,
+    label: "알림",
   },
   {
-    path:"/profile",
-    icon:<PersonIcon />,
-    label:"프로필"
+    path: "/profile",
+    icon: <PersonIcon />,
+    label: "프로필",
   },
   {
-    path:"/",
-    icon:<HomeIcon />,
-    label:"홈"
+    path: "/",
+    icon: <HomeIcon />,
+    label: "홈",
   },
   {
-    path:"/ranks",
-    icon:<StackedLineChartIcon />,
-    label:"순위"
+    path: "/ranks",
+    icon: <StackedLineChartIcon />,
+    label: "순위",
   },
   {
-    path:"/points",
-    icon:<AttachMoneyIcon />,
-    label:"포인트"
-  }
-]
+    path: "/points",
+    icon: <AttachMoneyIcon />,
+    label: "포인트",
+  },
+].map((value, index) => {
+  return { ...value, index };
+});
 
 export default function BottomNav() {
   const location = useLocation();
-  const [current, setCurrent] = useState(location.pathname.split("/")[0]);
-  
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setCurrent(newValue);
-  };
 
   return (
     <BottomNavigation
       showLabels
       className={tabBarStyle}
-      onChange={handleChange}
-      value={current}
+      value={tabBarRoutes.findIndex((value) => value.path == location.pathname)}
     >
-      {tabBarRoutes.map((value) => 
-        <BottomNavigationAction key={value.path.slice(1, -1)} label={value.label} icon={value.icon} component={Link} to={value.path} />
-      )}
+      {tabBarRoutes.map((value) => (
+        <BottomNavigationAction
+          key={value.path.slice(1, -1)}
+          label={value.label}
+          icon={value.icon}
+          component={Link}
+          to={value.path}
+        />
+      ))}
     </BottomNavigation>
   );
 }
