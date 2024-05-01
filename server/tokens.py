fdcae10f4e98b5bc1f526d946cf4f648a5949429
lnsqj123 <holmane33@gmail.com>
2024-05-02 01:21:44 +0900

    [back] feat: add auth decorator, get payload from header func

--- server/tokens.py
+++ server/tokens.py
@@ -1,8 +1,10 @@
 import jwt
+from flask import request, jsonify, make_response  # flask 관련 라이브러리
 import datetime
 from database import db
 import os
 from dotenv import load_dotenv
+from functools import wraps
 
 
 # 전처리
@@ -52,11 +54,86 @@ def check_refresh_token(refresh_token):
             payload = None
         else:
         """
-        userinfo = db.token.find_one({"user_id": payload["id"]}, {"_id": 0})
+        userinfo = db.token.find_one({"refresh_token": refresh_token}, {"_id": 0})
         # print(userinfo, refresh_token)
-        if userinfo is None or userinfo["refresh_token"] != refresh_token:
+        if userinfo is None:
             payload = None
+        else:
+            payload["id"] = userinfo["user_id"]
     except jwt.InvalidTokenError:  # 토큰이 만료된 경우 또는 없는 경우
         payload = None
 
     return payload
+
+
+# decorator for auth
+def token_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        token = request.headers.get("Cookie")
+        if token is not None:  # 토큰이 있는 경우
+            # access token 유효성 확인
+            access_token = (token.split("; ")[0]).split("=")[1]
+            if len(token.split("; ")) > 1:
+                refresh_token = (token.split("; ")[1]).split("=")[1]
+            payload_access = check_access_token(access_token)
+            # print(payload_access)
+            if payload_access is None and len(token.split("; ")) > 1:
+                # refresh token 유효성 확인
+                payload_refresh = check_refresh_token(refresh_token)
+                if payload_refresh is None:
+                    # access, refresh token decode 실패 시 401 반환
+                    return make_response(
+                        jsonify(
+                            {
+                                "result": "fail",
+                                "msg": "access token, refresh token이 없습니다.",
+                            }
+                        ),
+                        401,
+                    )
+            elif payload_access is None:
+                return make_response(
+                    jsonify(
+                        {
+                            "result": "fail",
+                            "msg": "access token, refresh token이 없습니다.",
+                        }
+                    ),
+                    401,
+                )
+        else:  # 토큰이 없는 경우 401 반환
+            return make_response(
+                jsonify(
+                    {"result": "fail", "msg": "access token, refresh token이 없습니다."}
+                ),
+                401,
+            )
+        return func(*args, **kwargs)
+
+    return decorated_function
+
+
+# header에 있는 token으로부터 payload 정보 가져오기
+def get_payload_from_header():
+    token = request.headers.get("Cookie")
+    if token is not None:  # 토큰이 있는 경우
+        # access token 유효성 확인
+        access_token = (token.split("; ")[0]).split("=")[1]
+        if len(token.split("; ")) > 1:
+            refresh_token = (token.split("; ")[1]).split("=")[1]
+        payload_access = check_access_token(access_token)
+        # print(payload_access)
+        if payload_access is not None:
+            return payload_access
+        if payload_access is None and len(token.split("; ")) > 1:
+            # refresh token 유효성 확인
+            payload_refresh = check_refresh_token(refresh_token)
+            if payload_refresh is not None:
+                # access, refresh token decode 실패 시 401 반환
+                return payload_refresh
+            return None
+        else:
+            return None
+    else:  # 토큰이 없는 경우 401 반환
+        return None
