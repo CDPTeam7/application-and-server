70a3a8b567818b7c5ea529d44f7695ca682decf5
blu3piece <kcw2205@naver.com>
2024-05-04 16:58:59 +0900

    [front] refactor: remove `useUserStore`
    - reason
    
    1. userStore gets more resources.
    2. Auth and User data has close relationship

--- client/src/api/auth/index.ts
+++ client/src/api/auth/index.ts
@@ -1,22 +1,22 @@
-import { axiosWithCookie } from "@/common/axios";
+import { axiosWithCookie } from "@/utils/axios";
 import axios from "axios";
 
 // 로그인 시 패킷을 중간에 열어볼 수 있어서, RSA 암호화 필요
 
-export function signUp(userId:string, password:string) {
+export function requestSignUp(userId: string, password: string) {
   return axios.post("/api/auth/signup", {
-    user_id:userId,
-    user_pw:password
+    user_id: userId,
+    user_pw: password,
   });
 }
 
-export function login(userId:string, password:string) {
+export function requestLogin(userId: string, password: string) {
   return axios.post("/api/auth/login", {
-      user_id:userId,
-      user_pw:password
+    user_id: userId,
+    user_pw: password,
   });
 }
 
-export function checkToken() {
+export function requestCheckToken() {
   return axiosWithCookie.post("/api/auth/checkToken");
 }
--- client/src/common/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { TOKEN_ACCESS_ID, TOKEN_REFRESH_ID, getCookie } from "./cookie";
-
-export const axiosWithCookie = axios.create({
-  headers:{
-    accessToken:await getCookie(TOKEN_ACCESS_ID),
-    refreshToken:await getCookie(TOKEN_REFRESH_ID)
-  }
-})
