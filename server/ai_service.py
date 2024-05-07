1712d324023f5747a8aed540ff6f86b384f1869a
lnsqj123 <holmane33@gmail.com>
2024-05-07 22:35:26 +0900

    [back] feat: create pet, face recognition API

--- server/ai_service.py
+++ server/ai_service.py
@@ -24,7 +24,7 @@ Ai_service = Namespace(
 )
 
 
-pet_model = CNN()
+"""pet_model = CNN()
 pet_model.load_state_dict(torch.load("./server/model/mnist_model.pt"), strict=False)
 pet_model.eval()
 # model.train(data='data.yaml', epochs=70, batch=-1, plots=True,imgsz=640, patience=10, save_period=5, mixup=0.25, label_smoothing=0.1, project='final', name='final')
@@ -32,26 +32,26 @@ pet_model.eval()
 # path = model.export()  # export the model to PT format
 normalize = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-)
+)"""
 
 
-pet_inference_fields = Ai_service.model(
-    "Pet_Inference_request",
+pet_recognition_fields = Ai_service.model(
+    "Pet_recognition_request",
     {  # Model 객체 생성
         "images": fields.String(
             description="an Images", required=True, example="np.array(image).tolist()"
         )
     },
 )
-pet_inference_response1 = Ai_service.model(
-    "Pet_Inference_success",
+pet_recognition_response1 = Ai_service.model(
+    "Pet_recognition_success",
     {
         "result": fields.String(example="success"),
         "data": fields.String(example="pet_bottle"),
     },
 )
-pet_inference_response2 = Ai_service.model(
-    "Pet_Inference_fail",
+pet_recognition_response2 = Ai_service.model(
+    "Pet_recognition_fail",
     {
         "result": fields.String(example="fail"),
         "msg": fields.String(example="평가할 수 없습니다."),
@@ -60,89 +60,58 @@ pet_inference_response2 = Ai_service.model(
 
 
 # pet bottle inference API
-@Ai_service.route("/petInference")
-class pet_inference(Resource):
-    @Ai_service.expect(pet_inference_fields)
-    @Ai_service.response(202, "success", pet_inference_response1)
-    @Ai_service.response(401, "fail", pet_inference_response2)
+@Ai_service.route("/pet-recognition")
+class pet_recognition(Resource):
+    @Ai_service.expect(pet_recognition_fields)
+    @Ai_service.response(200, "success", pet_recognition_response1)
+    @Ai_service.response(400, "fail", pet_recognition_response2)
     def post(self):
-        """입력 이미지에 대해 평가합니다. - 보강 필요"""
-        data = request.get_json()
-        _, result = pet_model.forward(
-            normalize(np.array(data["images"], dtype=np.uint8)).unsqueeze(0)
-        ).max(1)
+        """입력된 페트병 이미지에 대해 개수를 구해줍니다."""
+        frame = request.files["image"]
+        result = image_detection(frame)
         return make_response(
-            jsonify({"result": "success", "data": str(result.item())}), 202
+            jsonify({"result": "SUCCESS_PET_RECOGNITION", "data": {"count": result}}),
+            202,
         )
 
 
-pet_multi_inference_fields = Ai_service.inherit(
-    "Pet_Multi_Inference_request",
-    pet_inference_fields,
-    {},
-)
-pet_multi_inference_response1 = Ai_service.inherit(
-    "Pet_Multi_Inference_success",
-    pet_inference_response1,
-    {
-        "data": fields.String(example="3"),
-    },
-)
-pet_multi_inference_response2 = Ai_service.inherit(
-    "Pet_Multi_Inference_fail",
-    pet_inference_response2,
-    {
-        "msg": fields.String(example="평가할 수 없습니다."),
-    },
-)
-
-
-# pet bottle inference API
-@Ai_service.route("/petMultiInference")
-class pet_multi_inference(Resource):
-    @Ai_service.expect(pet_multi_inference_fields)
-    @Ai_service.response(202, "success", pet_multi_inference_response1)
-    @Ai_service.response(401, "fail", pet_multi_inference_response2)
+@Ai_service.route("/face-recognition")
+class face_recognition(Resource):
     def post(self):
-        """입력 이미지에 대해 페트병이 얼마나 있는지를 평가합니다. - 미구현"""
-        data = request.get_json()
-        _, result = pet_model.forward(
-            normalize(np.array(data["images"], dtype=np.uint8)).unsqueeze(0)
-        ).max(1)
-        return make_response(
-            jsonify({"result": "success", "data": str(result.item())}), 202
-        )
-
-
-@Ai_service.route("/process-frame", methods=["POST"])
-class process_frame(Resource):
-    def post(self):
-        data = request.json
-        frame = data.get("frame")
+        """입력된 얼굴 이미지에 대해 인증을 확인하여 로그인 여부를 구해줍니다."""
+        frame = request.files["image"]
+        frame.save("./static/face-recog/" + secure_filename(str(frame.filename)))
+        f = cv2.imread(("./static/face-recog/" + secure_filename(str(frame.filename))))
 
         try:
-            target_embedding = get_target_embedding(frame)
+            target_embedding = get_target_embedding(f)
             results = search_similar_images(target_embedding)
+            res = {}
             for result in results:
-                final_result = verify(target_embedding, result["embedding"])
+                final_result = verify(target_embedding, result["image"])
                 # print(result["img_path"])
+                res = result
                 break
             if final_result:
                 print("Face verification succeeded.")
                 response = {
-                    "result": "success",
+                    "result": "SUCCESS_LOGIN",
                     "msg": "얼굴 인식 성공 및 로그인 성공하였습니다.",
+                    "data": {"user_id": res["user_id"]},
                 }
-                return make_response(jsonify(response), 202)
+                return make_response(jsonify(response), 200)
             else:
                 print("Face verification failed.")
                 response = {
-                    "result": "fail",
+                    "result": "ERROR_FAIL_LOGIN",
                     "msg": "얼굴 인식 성공 및 로그인 실패하였습니다.",
                 }
-                return make_response(jsonify(response), 203)
+                return make_response(jsonify(response), 202)
         except Exception as e:
             print(e)
             print("No face detected in the target imaget or Error occured.")
-            response = {"result": "fail", "msg": "얼굴 인식 실패하였습니다."}
-            return make_response(jsonify(response), 401)
+            response = {
+                "result": "ERROR_FAIL_FACE_RECOGNITION",
+                "msg": "얼굴 인식 실패하였습니다.",
+            }
+            return make_response(jsonify(response), 400)
